b29b55271e60d2d18587a5adfdac167367c37814
raeeskasim1 <raeeskasim84@gmail.com>
2026-08-07 00:28:57 +0530

    Add misclassified image visualization

--- src/visualize.py
+++ src/visualize.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import pickle
 import torch
+import random
 
 from PIL import Image
-from dataset import train_loader, CLASS_NAMES, transform, NUM_CLASSES
+from dataset import train_loader, CLASS_NAMES, transform, NUM_CLASSES,test_loader
 from model.cnn import SceneVisionCNN
 from pathlib import Path
 
@@ -89,29 +90,36 @@ def plot_training_curves():
 
     plt.show()
 
-device = torch.device(
-    "cuda" if torch.cuda.is_available() else "cpu"
-)
+def get_feature_maps():
 
-model = SceneVisionCNN(NUM_CLASSES).to(device)
-
-model.load_state_dict(
-    torch.load(
-        "outputs/checkpoints/best_model.pth",
-        map_location=device
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else "cpu"
     )
-)
 
-model.eval()
+    model = SceneVisionCNN(NUM_CLASSES).to(device)
 
-image_path = Path("data") / "intel" / "seg_pred" / "10004.jpg"
+    model.load_state_dict(
+        torch.load(
+            "outputs/checkpoints/best_model.pth",
+            map_location=device
+        )
+    )
 
-image = Image.open(image_path).convert("RGB")
-image = transform(image)
-image = image.unsqueeze(0).to(device)
+    model.eval()
 
-with torch.no_grad():
-    outputs, fm1, fm2, fm3 = model(image)
+    image_path = Path("data") / "intel" / "seg_pred" / "10004.jpg"
+
+    image = Image.open(image_path).convert("RGB")
+
+    image = transform(image)
+
+    image = image.unsqueeze(0).to(device)
+
+    with torch.no_grad():
+
+        _, fm1, fm2, fm3 = model(image)
+
+    return fm1, fm2, fm3
 
 
 def visualize_feature_maps(feature_maps, block_name):
@@ -137,10 +145,108 @@ def visualize_feature_maps(feature_maps, block_name):
 
     plt.show()
 
+def visualize_misclassified_images():
+
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else "cpu"
+    )
+
+    model = SceneVisionCNN(NUM_CLASSES).to(device)
+
+    model.load_state_dict(
+        torch.load(
+            "outputs/checkpoints/best_model.pth",
+            map_location=device
+        )
+    )
+
+    model.eval()
+
+    misclassified_images = []
+    actual_labels = []
+    predicted_labels = []
+    confidences = []
+
+    with torch.no_grad():
+
+        for images, labels in test_loader:
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs, _, _, _ = model(images)
+
+            probabilities = torch.softmax(outputs, dim=1)
+
+            confidence, predicted = torch.max(
+                probabilities,
+                dim=1
+            )
+
+            mask = predicted != labels
+
+            misclassified_images.extend(
+                images[mask].cpu()
+            )
+
+            actual_labels.extend(
+                labels[mask].cpu()
+            )
+
+            predicted_labels.extend(
+                predicted[mask].cpu()
+            )
+
+            confidences.extend(
+                confidence[mask].cpu()
+            )
+
+    plt.figure(figsize=(12, 12))
+
+    indices = random.sample(
+        range(len(misclassified_images)),
+        min(16, len(misclassified_images))
+    )
+
+    for plot_idx, sample_idx in enumerate(indices):
+
+        plt.subplot(4, 4, plot_idx + 1)
+
+        image = misclassified_images[sample_idx].permute(1, 2, 0)
+
+        plt.imshow(image)
+
+        actual = CLASS_NAMES[actual_labels[sample_idx]]
+
+        predicted = CLASS_NAMES[predicted_labels[sample_idx]]
+
+        confidence_score = confidences[sample_idx].item() * 100
+
+        plt.title(
+            f"A: {actual}\n"
+            f"P: {predicted}\n"
+            f"{confidence_score:.1f}%",
+            fontsize=8
+        )
+
+        plt.axis("off")
+
+    plt.tight_layout()
+
+    plt.savefig(
+        "assets/misclassified_images_v1.png",
+        dpi=300,
+        bbox_inches="tight"
+    )
+
+    plt.show()
 
 if __name__ == "__main__":
 
     # plot_training_curves()
-    visualize_feature_maps(fm1, "block1")
-    visualize_feature_maps(fm2, "block2")
-    visualize_feature_maps(fm3, "block3")
+    # fm1, fm2, fm3 = get_feature_maps()
+    # visualize_feature_maps(fm1, "block1")
+    # visualize_feature_maps(fm2, "block2")
+    # visualize_feature_maps(fm3, "block3")
+
+    visualize_misclassified_images()
